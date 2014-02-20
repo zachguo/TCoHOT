8c9cde9855260272bd7929314b1c807738815261
Zach Guo <zachguo@gmail.com>
2014-02-19 20:27:57 -0500

    Fixes #4

--- metadata_processing/xml2json.py
+++ metadata_processing/xml2json.py
@@ -1,61 +1,21 @@
 #!/usr/bin/env python
 
-"""
-Modified to 
+"""Adapted from [xml2json.py](https://github.com/hay/xml2json) to
 1) keep attribute tag when striping namespace;
 2) abandon json2xml functionality;
-3) return sorted json even without '--pretty' option.
+3) use mongoDB default formatted JSON (one document perline);
+4) ONLY WORK FOR HATHITRUST XML METADATA;
 """
 
-"""xml2json.py  Convert XML to JSON
-
-Relies on ElementTree for the XML parsing.  This is based on
-pesterfish.py but uses a different XML->JSON mapping.
-The XML->JSON mapping is described at
-http://www.xml.com/pub/a/2006/05/31/converting-between-xml-and-json.html
-
-Rewritten to a command line utility by Hay Kranen < github.com/hay > with
-contributions from George Hamilton (gmh04) and Dan Brown (jdanbrown)
-
-XML                              JSON
-<e/>                             "e": null
-<e>text</e>                      "e": "text"
-<e name="value" />               "e": { "@name": "value" }
-<e name="value">text</e>         "e": { "@name": "value", "#text": "text" }
-<e> <a>text</a ><b>text</b> </e> "e": { "a": "text", "b": "text" }
-<e> <a>text</a> <a>text</a> </e> "e": { "a": ["text", "text"] }
-<e> text <a>text</a> </e>        "e": { "#text": "text", "a": "text" }
-
-This is very similar to the mapping used for Yahoo Web Services
-(http://developer.yahoo.com/common/json.html#xml).
-
-This is a mess in that it is so unpredictable -- it requires lots of testing
-(e.g. to see if values are lists or strings or dictionaries).  For use
-in Python this could be vastly cleaner.  Think about whether the internal
-form can be more self-consistent while maintaining good external
-characteristics for the JSON.
-
-Look at the Yahoo version closely to see how it works.  Maybe can adopt
-that completely if it makes more sense...
-
-R. White, 2006 November 6
-
+"""Original License:
 Copyright (C) 2010-2013, Hay Kranen
-
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
-import json
-import optparse
-import sys
-import os
-
+import json, sys, re
 import xml.etree.cElementTree as ET
-
 
 def strip_tag(tag):
     strip_ns_tag = tag
@@ -65,27 +25,22 @@ def strip_tag(tag):
         tag = strip_ns_tag
     return tag
 
-
 def elem_to_internal(elem, strip_ns=1, strip=1):
     """Convert an Element into an internal dictionary (not JSON!)."""
-
     d = {}
     elem_tag = elem.tag
     if strip_ns:
         elem_tag = strip_tag(elem.tag)
     for key, value in list(elem.attrib.items()):
-            d['@' + key] = value
+            d['_' + key] = value
 
     # loop over subelements to merge them
     for subelem in elem:
         v = elem_to_internal(subelem, strip_ns=strip_ns, strip=strip)
-
         tag = subelem.tag
         if strip_ns:
             tag = strip_tag(subelem.tag)
-
         value = v[tag]
-
         try:
             # add to existing list for this tag
             d[tag].append(value)
@@ -103,10 +58,8 @@ def elem_to_internal(elem, strip_ns=1, strip=1):
             text = text.strip()
         if tail:
             tail = tail.strip()
-
     if tail:
         d['#tail'] = tail
-
     if d:
         # use #text element if other attributes exist
         if text:
@@ -116,74 +69,30 @@ def elem_to_internal(elem, strip_ns=1, strip=1):
         d = text or None
     return {elem_tag: d}
 
-def elem2json(elem, options, strip_ns=1, strip=1):
-
+def elem2json(elem, strip_ns=1, strip=1):
     """Convert an ElementTree or Element into a JSON string."""
-
     if hasattr(elem, 'getroot'):
         elem = elem.getroot()
+    return json.dumps(elem_to_internal(elem, strip_ns=strip_ns, strip=strip), sort_keys=True)
 
-    if options.pretty:
-        return json.dumps(elem_to_internal(elem, strip_ns=strip_ns, strip=strip), sort_keys=True, indent=4, separators=(',', ': '))
-    else:
-        return json.dumps(elem_to_internal(elem, strip_ns=strip_ns, strip=strip), sort_keys=True)
-
-def xml2json(xmlstring, options, strip_ns=1, strip=1):
-
+def xml2json(xmlstring, strip_ns=1, strip=1):
     """Convert an XML string into a JSON string."""
-
     elem = ET.fromstring(xmlstring)
-    return elem2json(elem, options, strip_ns=strip_ns, strip=strip)
+    jout = elem2json(elem, strip_ns=strip_ns, strip=strip)
+    # remove unnecessary enclosing tags, and convert into mongoDB default format
+    jout = re.subn(r"(?<=\})\,\s+(?=\{\"\_id)", "\n", jout[24:len(jout)-3])[0]
+    return jout
 
 def main():
-    p = optparse.OptionParser(
-        description='Converts XML to JSON or the other way around.  Reads from standard input by default, or from file if given.',
-        prog='xml2json',
-        usage='%prog -t xml2json -o file.json [file]'
-    )
-    p.add_option('--type', '-t', help="'xml2json'", default="xml2json")
-    p.add_option('--out', '-o', help="Write to OUT instead of stdout")
-    p.add_option(
-        '--strip_text', action="store_true",
-        dest="strip_text", help="Strip text for xml2json")
-    p.add_option(
-        '--pretty', action="store_true",
-        dest="pretty", help="Format JSON output so it is easier to read")
-    p.add_option(
-        '--strip_namespace', action="store_true",
-        dest="strip_ns", help="Strip namespace for xml2json")
-    p.add_option(
-        '--strip_newlines', action="store_true",
-        dest="strip_nl", help="Strip newlines for xml2json")
-    options, arguments = p.parse_args()
-
-    inputstream = sys.stdin
-    if len(arguments) == 1:
-        try:
-            inputstream = open(arguments[0])
-        except:
-            sys.stderr.write("Problem reading '{0}'\n".format(arguments[0]))
-            p.print_help()
-            sys.exit(-1)
-
-    input = inputstream.read()
-
-    strip = 0
-    strip_ns = 0
-    if options.strip_text:
-        strip = 1
-    if options.strip_ns:
-        strip_ns = 1
-    if options.strip_nl:
-       input = input.replace('\n', '').replace('\r','')
-    if (options.type == "xml2json"):
-        out = xml2json(input, options, strip_ns, strip)
-    if (options.out):
-        file = open(options.out, 'w')
-        file.write(out)
-        file.close()
+    if len(sys.argv) != 3:
+        print "Please provide input and output filenames. Input filename first, output filename second."
+        sys.exit()
     else:
-        print(out)
+        infilename,outfilename = sys.argv[1:]
+        with open(infilename, 'r') as fin:
+            out = xml2json(fin.read(), strip_ns=1, strip=1)
+        with open(outfilename, 'w') as fout:
+            fout.write(out)
 
 if __name__ == "__main__":
     main()
