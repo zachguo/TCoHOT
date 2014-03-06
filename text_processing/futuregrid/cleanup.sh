87c6fd8bf98cc7ecbd2e177490fbbdb65ea42c7e
Zach Guo <zachguo@gmail.com>
2014-03-06 13:48:58 -0500

    init shell script for running codes on futuregrid

---
+++ text_processing/futuregrid/cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Created by Siyuan Guo, Mar 2014
+# Put this shell script in your $HOME directory
+# Clean up working directory after each run of hadoop
+
+rm HTRCDatePBS.log
+rm dateStreaming.sh
+rm -r myHadoop-config
+rm -r HTRCDateOutputFiles
