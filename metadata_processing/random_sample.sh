0cdeccd4104f8ba50487331261a162770c8ee855
Trevor Edelblute <tedelblu@indiana.edu>
2014-03-05 19:20:50 -0500

    added random sampling one liner

---
+++ metadata_processing/random_sample.sh
@@ -0,0 +1,6 @@
+# use this one line bash.sh perl command to randomly sample lines from a flat file
+# modify gt value to return that percentage of total lines, here 85%
+# replace 'source_file.txt' and 'out_file.txt' with your source data and output file respectively
+
+perl -ne 'print if (rand() < .85)' source_file.txt > out_file.txt
+
