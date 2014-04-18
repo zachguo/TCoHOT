eba874645ad085f697aee8735a3849f5270dc84a
Zach Guo <zachguo@gmail.com>
2014-04-18 15:22:23 -0400

    mapreduce codes can now run on OSX using parallel without error

--- text_processing/map_reduce/runLocalParallel.sh
+++ text_processing/map_reduce/runLocalParallel.sh
@@ -4,12 +4,21 @@
 
 # Run map reduce codes locally with GNU Parallel, please install parallel first.
 
+# This script receives 2 arguments, one is corpora path, the other one is MapReduce codes path.
+# To run:
+#	./runLocalParallel.sh path/to/aa path/to/getTF > tf_aa.txt
+# Or:
+#	./runLocalParallel.sh path/to/aa path/to/getDateInText > date_aa.txt
+
 # Change paths below before running the script
-export HTRC_TEXT_CONCAT_PATH=/Users/syg/Documents/corpora/hathitrust/corpora/concat_data/aa
-# export MAPREDUCE_CODES_PATH=/Users/syg/Dropbox/Projects/Z604-Project/text_processing/map_reduce/getDateInText
-export MAPREDUCE_CODES_PATH=/Users/syg/Dropbox/Projects/Z604-Project/text_processing/map_reduce/getTF
+export HTRC_TEXT_CONCAT_PATH=$1
+export MAPREDUCE_CODES_PATH=$2
 
 chmod +x $MAPREDUCE_CODES_PATH/mapper.py
 chmod +x $MAPREDUCE_CODES_PATH/reducer.py
 
-parallel 'export map_input_file=fakepath/{}; cat {} | $MAPREDUCE_CODES_PATH/mapper.py | sort | $MAPREDUCE_CODES_PATH/reducer.py' ::: $HTRC_TEXT_CONCAT_PATH/*.txt
+# To work in Ubuntu
+# parallel --gnu 'export map_input_file=fakepath/{}; cat {} | $MAPREDUCE_CODES_PATH/mapper.py | sort | $MAPREDUCE_CODES_PATH/reducer.py' ::: $HTRC_TEXT_CONCAT_PATH/*.txt
+
+# To work in OSX (without argument list too long error)
+ls $HTRC_TEXT_CONCAT_PATH | grep \.txt | parallel 'export map_input_file=fakepath/{}; cat $HTRC_TEXT_CONCAT_PATH/{} | $MAPREDUCE_CODES_PATH/mapper.py | sort | $MAPREDUCE_CODES_PATH/reducer.py'
